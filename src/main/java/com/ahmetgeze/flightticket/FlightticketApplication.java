0664c95e1c31ae9057c3ec95bdbc6001c7bee6f9
ahmetserdargeze <gezeahmetserdar@gmail.com>
2020-09-05 10:27:07 +0300

    İnitial Commit

---
+++ src/main/java/com/ahmetgeze/flightticket/FlightticketApplication.java
@@ -0,0 +1,13 @@
+package com.ahmetgeze.flightticket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightticketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlightticketApplication.class, args);
+    }
+
+}
